939c31e1095afbd9d8008db0117b6cfc8deffa96
Golembieski <BillGolembieski@projectu23.com>
2016-09-08 00:28:04 -0400

    main.py commit 09/08/2016

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 from flask import Flask
+from get_wind_data import *
+
+app = Flask(__name__)
+
+#routing tying a URL to a python function
+@app.route('/')
+def index():
+    return get_wind('Cicero', 'NY')
+
+# Check to make sure we only run the webserver when this file is run directly
+if __name__ == "__main__":
+    app.run(debug=True)
