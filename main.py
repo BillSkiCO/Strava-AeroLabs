e378604aeb09c48ab08f1d9a115872b1db1190c6
Golembieski <BillGolembieski@projectu23.com>
2016-09-11 16:35:01 -0400

    Bootstrap addition

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from get_wind_data import get_wind
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="X:/AeroLabs/Strava-AeroLabs/static")
 
 #routing tying a URL to a python function
 @app.route('/')
