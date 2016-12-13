a918eb1c579c3301ad860b5e7bb8d6226fea9382
Golembieski <BillGolembieski@projectu23.com>
2016-12-13 14:55:47 -0500

    Float fix

--- test_calculate.py
+++ test_calculate.py
@@ -41,16 +41,16 @@ class TestCalculate(unittest.TestCase):
     # Longs: +- 180
 
         # Check north bearing in all quadrants x == 0
-        test_bearing_nw_quad = calculate.bearing(45, -90, 46, -90)
+        test_bearing_nw_quad = calculate.bearing(45.0, -90.0, 46.0, -90.0)
         self.assertTrue(self, test_bearing_nw_quad == 0, msg='calculate.bearing(NW Quad) not returning north')
 
-        test_bearing_ne_quad = calculate.bearing(45, 90, 46, 90)
+        test_bearing_ne_quad = calculate.bearing(45.0, 90.0, 46.0, 90.0)
         self.assertTrue(self, test_bearing_ne_quad == 0, msg='calculate.bearing(NE Quad) not returning north')
 
-        test_bearing_sw_quad = calculate.bearing(-45, -90, -44, -90)
+        test_bearing_sw_quad = calculate.bearing(-45.0, -90.0, -44.0, -90.0)
         self.assertTrue(self, test_bearing_sw_quad == 0, msg='calculate.bearing(SW Quad) not returning north')
 
-        test_bearing_se_quad = calculate.bearing(-45, 90, -44, 90)
+        test_bearing_se_quad = calculate.bearing(-45.0, 90.0, -44.0, 90.0)
         self.assertTrue(self, test_bearing_se_quad == 0, msg='calculate.bearing(SE Quad) not returning north')
 
         # Check northeast bearing 0 < x < 90
